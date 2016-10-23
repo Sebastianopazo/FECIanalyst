551db9a848068c6e75d78b94b594622b49f3db8a
Sebastian Opazo <sropazo@uc.cl>
2016-10-23 15:49:25 -0400

    more script fixes

---
+++ assets/js/youtubeSearch.js
@@ -0,0 +1,20 @@
+  $(function () {
+    $("form").on("submit", function (e) {
+      e.preventDefault();
+      var request = gapi.client.youtube.search.list({
+        part: "snippet",
+        type: "video",
+        q: encondeURIComponent($("#search").val()).replace(/%20/g, "+"),
+        maxResults: 10,
+        order: "relevance",
+      });
+      request.execute(function(response) {
+        console.log(response);
+      });
+    });
+  });
+function init () {
+gapi.client.setApiKey("AIzaSyBx4OVnRKhXRdb7LxdeM4FkWDqNKlRN_fw");
+gapi.client.load("youtube", v3, function() {
+});
+}
